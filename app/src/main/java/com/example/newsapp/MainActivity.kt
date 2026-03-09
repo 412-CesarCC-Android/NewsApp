92746f03350870cbc060c04adbaa444c1bfd9353
SHW378 <cesarbrmn@hotmail.com>
2026-03-09 00:08:54 -0600

    models, base main

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PantallasNoticias(innerPadding)
                 }
             }
         }
@@ -31,17 +33,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PantallasNoticias(innerPadding: PaddingValues) {
+    val colors = MaterialTheme.colorScheme
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(colors.background)
+        .padding(innerPadding)
+        .padding(horizontal = 16.dp)
+    ) {
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+
 @Composable
-fun GreetingPreview() {
+fun PantallaNoticiasPreview() {
     NewsAppTheme {
-        Greeting("Android")
+        PantallasNoticias(innerPadding = PaddingValues(0.dp))
     }
 }
